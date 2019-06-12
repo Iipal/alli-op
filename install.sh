1eaf9d6ce05dca50e0a1de5ef8a6b6425ac614c2
Iipal <malkoleyplay@gmail.com>
2019-06-12 12:52:33 +0300

    Improved install script

--- install.sh
+++ install.sh
@@ -1,1 +1,27 @@
-cp alli-op ~/.alli-op && echo "source ~/.alli-op" >> ~/.zshrc
+#!/bin/bash
+
+echo "Configure alli-op for your sytem..."
+UNAME_S=$(uname -s)
+if [ -z "$UNAME_S" ]; then
+    echo "Something went wrong, maybe you dont have \'uname\' command ?"
+elif [ "$UNAME_S"  == "Darwin" ]; then
+    sed -i -e "alias open" alli-op
+    sed -i -e "alias code" alli-op
+elif [ "$UNAME_S" == "Linus" ]; then
+    sed '4,8d' alli-op
+    sed '13,18d' alli-op
+fi
+
+echo "Copying alli-op to ~/.alli-op..."
+cp alli-op ~/.alli-op
+
+echo "Adding ~/.alli-op to your ~/.*shell_name*rc..."
+if [ "$SHELL" == "/bin/zsh" ]; then
+    echo "source ~/.alli-op" >> ~/.zshrc
+elif [ "$SHELL" == "/bin/bash" ]; then
+    echo "source ~/.alli-op" >> ~/.bashrc
+else
+    echo "ERROR: Supports only ZSH and bash. Try to add \"source ~/.alli-op\" by yourself in your ~/.*shell_name*rc"
+fi
+
+echo "Done. Please restart your shell."

0b89bf98b739d9408fe4b09c43840901433d224c
Iipal <malkoleyplay@gmail.com>
2019-06-17 10:33:42 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ UNAME_S=$(uname -s)
 if [ -z "$UNAME_S" ]; then
     echo "Something went wrong, maybe you dont have \'uname\' command ?"
 elif [ "$UNAME_S"  == "Darwin" ]; then
-    sed '41,42d' alli-op > .alli-op-tmp
+    sed '42,43d' alli-op > .alli-op-tmp
 elif [ "$UNAME_S" == "Linux" ]; then
     sed '4,7d' alli-op > .alli-op-tmp
     sed '9,13d' .alli-op-tmp > .alli-op-tmp2
@@ -19,10 +19,10 @@ cp .alli-op-tmp ~/.alli-op
 
 echo -n "Adding ~/.alli-op to your "
 if [[ "$SHELL" == "/bin/zsh" || "$SHELL" == "/usr/bin/zsh" ]]; then
-    echo "~/.zshrc..."
+    echo "~/.zshrc ..."
     echo "source ~/.alli-op" >> ~/.zshrc
 elif [[ "$SHELL" == "/bin/bash" || "$SHELL" == "/usr/bin/bash" ]]; then
-    echo "~/.bashrc..."
+    echo "~/.bashrc ..."
     echo "source ~/.alli-op" >> ~/.bashrc
 else
     echo "ERROR: Supports only ZSH and bash. Try to add \"source ~/.alli-op\" by yourself in your ~/.*shell_name*rc"
